bfa147ec07e7d8dff1e8969a49209e8ff02a0abf
Maruf Berdiyev <93835738+fuga02@users.noreply.github.com>
2023-07-17 04:49:59 -0700

    course repository was added

---
+++ src/Course.Api/Repositories/ICourseRepository.cs
@@ -0,0 +1,71 @@
+﻿using Course.Api.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Course.Api.Repositories;
+
+public interface ICourseRepository
+{
+    Task<List<Entities.Course>> GetCourses();
+
+    Task AddCourse( Entities.Course course);
+
+    Task<Entities.Course> GetCourseById( Guid courseId);
+
+    Task UpdateCourse( Entities.Course course);
+
+    Task DeleteCourse( Guid courseId);
+
+
+}
+
+public class CourseRepository: ICourseRepository
+{
+    private readonly CourseDbContext _context;
+
+    public CourseRepository(CourseDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<Entities.Course>> GetCourses()
+    {
+        return await _context.Courses.ToListAsync();
+    }
+
+    public async Task AddCourse(Entities.Course course)
+    {
+        _context.Courses.Add(course);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<Entities.Course> GetCourseById(Guid courseId)
+    {
+        var course = await _context.Courses.FirstOrDefaultAsync(c => c.CourseId == courseId);
+        if (course == null) throw new Exception("Course Not Found");
+        return course;
+    }
+
+    public async Task UpdateCourse( Entities.Course course)
+    {
+        _context.Courses.Update(course);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteCourse(Guid courseId)
+    {
+        var course = await GetCourseById(courseId);
+        _context.Courses.Remove(course);
+        await _context.SaveChangesAsync();
+    }
+
+    
+}
+
+
+/*
+    GET    courses
+    POST   courses
+    GET    courses/{ course_id}
+    PUT    courses/{ course_id}
+    DELETE courses/{ course_id}
+*/
