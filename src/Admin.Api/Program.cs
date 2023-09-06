d16273d888b2fc03c17b01d22b4290bb21743eb1
Maruf Berdiyev <93835738+fuga02@users.noreply.github.com>
2023-09-06 12:26:05 -0700

    swaggaer UI was added for admin Api

--- src/Admin.Api/Program.cs
+++ src/Admin.Api/Program.cs
@@ -1,25 +1,39 @@
+using Admin.Api.Extensions;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
+builder.Services.AddSwaggerGenJwt();
+
+builder.Services.AddJwt(builder.Configuration);
+
+builder.Configuration.AddJsonFile("ocelot.json", false, false);
+builder.Configuration.AddJsonFile("swagger_configuration.json", false, false);
+builder.Services.AddOcelot(builder.Configuration);
+
+var configuration = builder.Configuration;
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerForOcelot(configuration);
 }
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+await app.UseOcelot();
 
 app.Run();
