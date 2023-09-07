9c4582c1d65cb216483019798e05f6336338c027
Maruf Berdiyev <93835738+fuga02@users.noreply.github.com>
2023-09-07 03:39:04 -0700

    there are a lot changes

--- src/Course.Api/Extensions/ServiceCollectionExtensions.cs
+++ src/Course.Api/Extensions/ServiceCollectionExtensions.cs
@@ -5,14 +5,27 @@ namespace Course.Api.Extensions;
 
 public static  class ServiceCollectionExtensions
 {
-   
 
-    public static void MigrateCourseDb(this WebApplication app)
+
+    /*public static void MigrateCourseDb(this WebApplication app)
     {
         if (app.Services.GetService<CourseDbContext>() != null)
         {
             var courseDb = app.Services.GetRequiredService<CourseDbContext>();
             courseDb.Database.Migrate();
+        }
+    }*/
+    public static void MigrateCourseDb(this IApplicationBuilder app)
+    {
+        using (var scope = app.ApplicationServices.CreateScope())
+        {
+            var services = scope.ServiceProvider;
+            var courseDb = services.GetService<CourseDbContext>();
+
+            if (courseDb != null)
+            {
+                courseDb.Database.Migrate();
+            }
         }
     }
 }
