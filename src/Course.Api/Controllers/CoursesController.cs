1c2dae268574d6b0d54cb1742e207e37da35259f
Maruf Berdiyev <93835738+fuga02@users.noreply.github.com>
2023-09-07 08:56:36 -0700

    course controller was filled

--- src/Course.Api/Controllers/CoursesController.cs
+++ src/Course.Api/Controllers/CoursesController.cs
@@ -1,4 +1,5 @@
-﻿using Course.Api.Models.CourseModels;
+﻿using Course.Api.Managers;
+using Course.Api.Models.CourseModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Course.Api.Controllers;
@@ -6,34 +7,42 @@ namespace Course.Api.Controllers;
 [ApiController]
 public class CoursesController : ControllerBase
 {
+    private readonly CourseManager _courseManager;
+
+    public CoursesController(CourseManager courseManager)
+    {
+        _courseManager = courseManager;
+    }
+
     [HttpGet]
     public async Task<IActionResult> GetAllCourses()
     {
-        return Ok();
+        var courses = await _courseManager.GetCourses();
+
+        return Ok(courses);
     }
 
     [HttpPost]
     public async Task<IActionResult> AddCourse(CreateCourseModel model)
     {
-
-        return Ok();
+        return Ok(await _courseManager.AddCourse(model));
     }
 
     [HttpGet("{courseId}")]
     public async Task<IActionResult> GetCourseById(Guid courseId)
     {
-        return Ok();
+        return Ok(await _courseManager.GetCourseById(courseId) );
     }
 
     [HttpPut("{courseId}")]
-    public async Task<IActionResult> UpdateCourseById(Guid courseId)
+    public async Task<IActionResult> UpdateCourseById(Guid courseId,CreateCourseModel model)
     {
-        return Ok();
+        return Ok(await _courseManager.UpdateCourse(courseId,model));
     }
 
     [HttpDelete("{courseId}")]
     public async Task<IActionResult> DeleteCourseById(Guid courseId)
     {
-        return Ok();
+        return Ok(await _courseManager.DeleteCourse(courseId));
     }
 }
